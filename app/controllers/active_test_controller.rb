cdfdd01b7944957a705f26e7c8755e446bb3e63b
EC2 Default User <ec2-user@ip-10-0-0-181.ap-northeast-1.compute.internal>
2019-09-26 08:06:12 +0000

    add easy check

--- app/controllers/active_test_controller.rb
+++ app/controllers/active_test_controller.rb
@@ -1,5 +1,14 @@
 class ActiveTestController < ApplicationController
+  before_action :check_param
   def index
+  end
+  
+  private
+  
+  def check_param
+    unless params[:pass] || params[:pass] == "uotani"
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    end
   end
 
 end
